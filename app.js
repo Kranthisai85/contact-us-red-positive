f3f5c5456faa65810082a56930311a6603c53bcd
KRANTHI DEV SAI <kranthisai85@gmail.com>
2021-06-17 00:34:00 +0530

    make it better

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get("/", (req, res) => {
 app.post('/contactUs',contactUs);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, 'localhost',() => {
+const host = '0.0.0.0';
+app.listen(PORT, host,() => {
   console.log(`Server is running on port ${PORT}.`);
 });
